cdc58d87b74bbb91ac3f35c1e9310df60ce36acb
Ahrorbek05 <akramjonovahrorbek@gmail.com>
2024-08-06 11:22:13 +0500

    homework done!

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import BarChart from './components/BarChart';
+import { CssBaseline } from '@mui/material';
+
+function App() {
+  return (
+    <div className="App">
+      <CssBaseline />
+      <BarChart />
+    </div>
+  );
+}
+
+export default App;
